15d5e7a74a791800949fc403519fedcc3d50a433
Kunal Bafna <bafnakunal3@gmail.com>
2021-10-20 21:11:42 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -9,12 +9,18 @@ export default function TextForm(props) {
         setText(newText);
     }
 
+    const handleLoClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+
     // can get the event target value
     const handleOnChange = (event)=>{
         setText(event.target.value);
     }
     
-    const [text, setText] = useState('initialState');
+    // useState syntax from the docs
+    const [text, setText] = useState('');
 
     return (
         <>
@@ -27,7 +33,18 @@ export default function TextForm(props) {
             
             </form>
             <br />
-            <button className="btn btn-primary" onClick={handleUpClick} >Convert to Uppercase</button>
+            <button className="btn btn-info mx-1" onClick={handleUpClick} >Convert to Uppercase</button>
+            <button className="btn btn-primary mx-1" onClick={handleLoClick} >Convert to Lowercase</button>
+
+            <div className="container my-3">
+                <h2>text summary</h2>
+                {/* to get words and char on the fly */}
+                {/* text.split will give the array and therefore .length for the length */}
+                <p>{text.split(" ").length} words and {text.length} characters.</p>
+                <p>{ 0.008 * text.split(" ").length } Minutes read.</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
         </>
     )
 }
